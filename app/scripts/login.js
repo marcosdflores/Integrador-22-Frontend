70fd3d47704f8f46b4d81284dfe59d281789a30f
Marcos Flores <marcosflores1796@gmail.com>
2023-09-28 13:27:44 -0300

    Actualización e incorporación de archivos.

---
+++ app/scripts/login.js
@@ -0,0 +1,34 @@
+document.getElementById("loginForm").addEventListener("submit", function (event) {
+    event.preventDefault();
+    login();
+});
+
+function login() {
+    const data = {
+        username: document.getElementById("username").value,
+        contrasena: document.getElementById("paswwordd").value,
+    };
+
+    fetch("http://127.0.0.1:5000/login", {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(data),
+        credentials: 'include'
+    })
+    .then(response => {
+        if (response.status === 200) {
+            return response.json().then(data => {
+                window.location.href = "profile.html";
+            });
+        } else {
+            return response.json().then(data => {
+                document.getElementById("message").innerHTML = data.message;
+            });
+        }
+    })
+    .catch(error => {
+        document.getElementById("message").innerHTML = "An error occurred.";
+    });
+}
---
+++ app/scripts/profile.js
@@ -0,0 +1,61 @@
+window.addEventListener("load", function () {
+    getProfile();
+});
+
+const editButton = document.getElementById("edit");
+
+editButton.addEventListener("click", function () {
+  window.location.href = "editar-perfil.html";
+});
+
+document.getElementById("logout").addEventListener("click", logout);
+
+function getProfile() {
+    const url = "http://127.0.0.1:5000/profile";
+    
+    fetch(url, {
+        method: 'GET',
+        credentials: 'include'
+    })
+    .then(response => {
+        if (response.status === 200) {
+            return response.json().then(data => {
+                document.getElementById("nombre").innerText = data.nombre;
+                document.getElementById("apellido").innerText = data.apellido;
+                document.getElementById("username").innerText = data.username;
+                document.getElementById("email").innerText = data.email;
+                document.getElementById("imagen").src = data.imagen;
+            });
+        } else {
+            return response.json().then(data => {
+                document.getElementById("message").innerHTML = data.message;
+            });
+        }
+    })
+    .catch(error => {
+        document.getElementById("message").innerHTML = "An error occurred.";
+    });
+}
+
+function logout() {
+    const url = "http://127.0.0.1:5000/logout";
+    
+    fetch(url, {
+        method: 'GET',
+        credentials: 'include'
+    })
+    .then(response => {
+        if (response.status === 200) {
+            return response.json().then(data => {
+                window.location.href = "login.html";
+            });
+        } else {
+            return response.json().then(data => {
+                document.getElementById("message").innerHTML = data.message;
+            });
+        }
+    })
+    .catch(error => {
+        document.getElementById("message").innerHTML = "An error occurred.";
+    });
+}
